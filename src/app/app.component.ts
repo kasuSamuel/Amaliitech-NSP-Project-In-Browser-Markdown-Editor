a71bbf397aa0824de160161fa57b9546ea97a931
Samuel kasu <kasusamuel17@gmail.com>
2024-07-03 22:27:42 +0000

    Initialized component with data initialization and theme setup in ngOnInit.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,10 +8,33 @@ import { DataService } from './data.service';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MarkdownModule, CommonModule, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
-export class AppComponent {
-  title = 'In-Browser-Markdown-Editor';
+export class AppComponent implements OnInit {
+  markdown = ``;
+  data: any[] = [];
+  selectedData: any;
+  isDark: boolean = false;
+
+  constructor(private dataService: DataService) {}
+
+  ngOnInit(): void {
+    this.dataService.fetchData().subscribe((data) => {
+      this.data = data;
+      this.selectedData = this.data[this.data.length - 1];
+    });
+
+    this.dataService.isSelected$.subscribe((doc) => {
+      this.selectedData = doc;
+    });
+
+    if (this.data.length > 0 && !this.selectedData) {
+      this.lastData();
+    }
+
+    this.isDark = localStorage.getItem('isDark') === 'dark';
+    this.themeToggle();
+  }
 }
