21cad933e3242e38eb1ae58c945c6e40fc9fafcb
Samuel kasu <kasusamuel17@gmail.com>
2024-07-03 22:33:22 +0000

    Created date formatting function formatDate for consistent date display.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -82,4 +82,12 @@ export class AppComponent implements OnInit {
   viewDocument(document: any) {
     this.dataService.setSelected(document);
   }
+
+  formatDate(date: Date): string {
+    const year = date.getFullYear().toString();
+    const month = (date.getMonth() + 1).toString().padStart(2, '0');
+    const day = date.getDate().toString().padStart(2, '0');
+
+    return `${day}-${month}-${year}`;
+  }
 }
