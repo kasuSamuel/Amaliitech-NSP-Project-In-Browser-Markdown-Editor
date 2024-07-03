59b89982db37e11adadee3280017db41c21beec2
Samuel kasu <kasusamuel17@gmail.com>
2024-07-03 22:23:37 +0000

    Added DataService Angular service for handling data operations including local storage management and fetching from JSON.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,16 @@
-import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MarkdownModule } from 'ngx-markdown';
+import { DataService } from './data.service';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'In-Browser-Markdown-Editor';
--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -1,9 +1,19 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-
+import { BehaviorSubject, Observable, of } from 'rxjs';
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class DataService {
+  private jsonUrl = 'assets/data.json';
+  private localStorageKey = 'document';
+  localData: any;
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  saveToLocalStorage(document: any[]) {
+    localStorage.setItem(this.localStorageKey, JSON.stringify(document));
+  }
+
+
 }
